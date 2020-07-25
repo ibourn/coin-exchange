bde157b1303d2c05008c92d9903da2c184354913
ibourn <36422029+ibourn@users.noreply.github.com>
2020-07-25 22:48:56 +0200

    componentization and dprop drilling

--- src/App.js
+++ src/App.js
@@ -1,76 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Coin from './components/Coins/Coin';
+import ExchangeHeader from './components/ExchangeHeader/ExchangeHeader';
+import CoinList from './components/CoinList/CoinList';
 import AccountBalance from './components/AccountBalance/AccountBalance';
-
-/*
-//usage snippet flop
-let sum = 0;
-for(let num of [0,1,2,3,4,5]) {
-  sum += num;
-}
-
-//usage prom => promise
-return new Promise((resolve, reject) => {
-  
-}).then((result) => {
-  
-}).catch((err) => {
-  
-});
+import styled from 'styled-components';
 
 
-//exemple usage bracket colorizer
-[1,2,3,4,5,6]
-  .map( (item, index) => {return { item: item, key: index}; })
-  .reduce( (accumulator, nextValue) => {
-    return accumulator + <li>${nextValue}</li>;
-  }, '');
+const DivApp = styled.div`
+    text-align: center;
+    background-color: rgb(20, 56, 97);
+    color: #cccccc;
+    `;
 
-*/
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      balance: 10000,
+      coinData: [
+        {
+          name: "Bitcoin",
+          ticker: 'BTC',
+          price: 9999.9
+        },
+        {
+          name: "Ethereum",
+          ticker: 'ETH',
+          price: 274.9
+        },
+        {
+          name: "Tether",
+          ticker: 'USDT',
+          price: 1
+        },
+        {
+          name: "Ripple",
+          ticker: 'XRP',
+          price: 0.2
+        },
+        {
+          name: "Bitcoin Cash",
+          ticker: 'BCH',
+          price: 298.99
+        },
+        {
+          name: "Ethereum",
+          ticker: 'ETH',
+          price: 274.9
+        },
+        {
+          name: "Ethereum",
+          ticker: 'ETH',
+          price: 274.9
+        }
+      ]
+    }
+  }
 
-  
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="React logo" />
-        <h1 className="App-title">
-          Coin Exchange
-        </h1>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
-      {/*ajout component balance*/}
-      <AccountBalance amount={10000} />
-      <table className="coin-table"> 
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Ticker</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>
-
-        <Coin name="Bitcoin" ticker='BTC' price={9999.9} />
-        <Coin name="Ethereum" ticker='ETH' price={274.9}/>
-        <Coin name="Tether" ticker='USDT' price={1}/>
-        <Coin name="Ripple" ticker='XRP' price={0.2}/>
-        <Coin name="Ethereum" ticker='ETH' price={274.9}/>
-        <Coin name="Ethereum" ticker='ETH' price={274.9}/>
-        <Coin name="Ethereum" ticker='ETH' price={274.9}/>
-      </tbody>
-    </table>
-    </div>
-  );
+  render() {
+    return (
+      <DivApp>
+        <ExchangeHeader />
+        <AccountBalance amount={this.state.balance} />
+        <CoinList coinData={this.state.coinData} />
+      </DivApp>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ExchangeHeader/ExchangeHeader.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import styled from 'styled-components';
+
+const Img = styled.img`
+    height: 8rem; /*40vmin;*/
+    pointer-events: none;
+    `;
+
+
+const Header = styled.header`
+    background-color: #282c34;
+    min-height: 20hv;/* 100vh;*/
+    display: flex;
+    flex-direction: row; /*column;*/
+    align-items: center;
+    justify-content: flex-start; /*center;*/
+    font-size: 36px; /*calc(10px + 2vmin);*/
+    color: white;
+  `;
+
+const H1 = styled.h1`
+    font-size: 4rem;
+    `;
+
+
+
+export default class ExchangeHeader extends Component {
+    render() {
+        return (
+            <Header>
+                <Img src={logo} alt="React logo" />
+                <H1>
+                    Coin Exchange
+                </H1>
+
+            </Header>
+        )
+    }
+}
